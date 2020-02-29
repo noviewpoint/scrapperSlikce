3ffaea0cd629b7cc6e5ebfd8363d3a338470447a
David Brvar <david.brvar@mmv.si>
2020-02-29 10:30:17 +0100

    posiljanje sporocil uporabnikom, zajebavaje z jsessionid, nadomestil rp z axios, razno

--- README.md
+++ README.md
@@ -6,10 +6,10 @@
 
 2. export sliciceUsername="marvin" && export daysOld=3 && node doit.js
 
-# scrapperSlikce v2
+# scrapperSlikce v2 (avtomatsko poslje tudi sporocila uporabnikom za izmenjavo slicic)
 
 0. install nodeJS (x64, .msi, default install options)
 
 1. run 'npm install'
 
-2. export sliciceNetUsername="marvin" && export sliciceNetAlbumId=61 && node doIt2.js 
+2. export sliciceNetUsername=marvin && sliciceNetPassword=xxx && export sliciceNetAlbumId=61 && export sliciceNetDoSendPrivateMessages=false && node doIt2.js
--- doIt2.js
+++ doIt2.js
@@ -1,11 +1,38 @@
-const rp = require("request-promise");
 const JSDOM = require("jsdom").JSDOM;
 const moment = require("moment");
 const cloneDeep = require("lodash.clonedeep");
+const axios = require("axios");
+const querystring = require("querystring");
+
+let JSESSIONID; // fak...
 
 const init = async () => {
+	handleCheckRequiredUserParameters();
+
+	const username = String(process.env.sliciceNetUsername);
+	const password = String(process.env.sliciceNetPassword);
+	const albumId = String(process.env.sliciceNetAlbumId);
+	const doSendPrivateMessages = Boolean(process.env.sliciceNetDoSendPrivateMessages === "true");
+
+	const results = await scrapeAlbum(albumId);
+	if (!results.has(username)) {
+		console.error(`Data for username '${username}' was not found on Slicice.net. Ending program...`);
+	}
+	const userData = results.get(username);
+	results.delete(username);
+	const trades = autoTrade({ results, userData });
+	await sendMessages({ trades, username, password, doSendPrivateMessages });
+	console.log("There are no more suitable cards to trade. Ending program...");
+};
+
+const handleCheckRequiredUserParameters = () => {
 	if (!process.env.sliciceNetUsername) {
 		console.error("Provide username for Slicice.net. Ending program...");
+		process.exit(0);
+	}
+
+	if (!process.env.sliciceNetPassword) {
+		console.error("Provide password for Slicice.net. Ending program...");
 		process.exit(0);
 	}
 
@@ -13,95 +40,149 @@ const init = async () => {
 		console.error("Provide albumId for Slicice.net. Ending program...");
 		process.exit(0);
 	}
-
-	const username = process.env.sliciceNetUsername;
-	const albumId = process.env.sliciceNetAlbumId;
-
-	const results = new Map();
-	await scrape({ results, albumId });
-	if (!results.has(username)) {
-		console.error(`Data for username '${username}' was not found on Slicice.net. Ending program...`);
-	}
-	const userData = results.get(username);
-	results.delete(username);
-	autoTrade({ results, userData });
-	console.log("There are no more suitable cards to trade. Ending program...");
 };
 
-const scrape = async ({ results, albumId, pagesCount = 0 }) => {
-	let response;
-	try {
-		const url = `http://slicice.net/search/albums.html?go=true&id=${albumId}&position=${pagesCount}`;
-		console.log(`Calling: ${url}`);
-		response = new JSDOM(await rp(url));
-	} catch {
-		return;
-	}
-	const elements = response.window.document.querySelectorAll("div.offersBrowserRight.fix.right");
-	for (const el of elements) {
-		const head = el.querySelector("div.offersBrowserHead.fix");
-		const body = el.querySelector("div.offersBrowserrEntry.fix");
-		const missing = body
-			.querySelector("p.missing")
-			.textContent.replace(/iščem: /g, "")
-			.replace(/\s/g, "");
-		const duplicates = body
-			.querySelector("p.duplicates")
-			.textContent.replace(/ponujam: /g, "")
-			.replace(/\s/g, "");
+const scrapeAlbum = async albumId => {
+	const results = new Map();
 
-		const res = {
-			username: head.querySelector("a.userName").textContent,
-			timestamp: moment(head.querySelector("span.offerDate").textContent, "DD-MM-YYYY").toDate(),
-			missing: new Set(
-				missing
-					.split(",")
-					.filter(el => el !== "")
-					.map(el => padZeros(el)),
-			),
-			duplicates: new Set(
-				duplicates
-					.split(",")
-					.filter(el => el !== "")
-					.map(el => padZeros(el)),
-			),
-		};
+	const firstPage = await scrapeAlbumPage({ albumId, pageNumber: 0 });
+	const otherPagesNumbers = getValidPagesNumbers({ albumId, page: firstPage });
+	const otherPages = [];
+	for (const num of otherPagesNumbers) {
+		otherPages.push(await scrapeAlbumPage({ albumId, pageNumber: num }));
+	}
+	const pages = [firstPage, ...otherPages];
 
-		results.set(res.username, res);
+	for (const page of pages) {
+		const elements = page.window.document.querySelectorAll("div.offersBrowserRight.fix.right");
+		for (const el of elements) {
+			const head = el.querySelector("div.offersBrowserHead.fix");
+			const body = el.querySelector("div.offersBrowserrEntry.fix");
+			const answer = el.querySelector("div.answer");
+
+			const username = head.querySelector("a.userName").textContent;
+			const profilePageLink = `http://slicice.net${head.querySelector("a.userName").href}`;
+			const messageLink = `http://slicice.net${answer.querySelector("a").href}`;
+			const timestamp = moment(head.querySelector("span.offerDate").textContent, "DD-MM-YYYY").toDate();
+			const missing = body
+				.querySelector("p.missing")
+				.textContent.replace(/iščem: /g, "")
+				.replace(/\s/g, "");
+			const duplicates = body
+				.querySelector("p.duplicates")
+				.textContent.replace(/ponujam: /g, "")
+				.replace(/\s/g, "");
+
+			const res = {
+				username,
+				profilePageLink,
+				messageLink,
+				timestamp,
+				missing: new Set(
+					missing
+						.split(",")
+						.filter(el => el !== "")
+						.map(el => padZeros(el)),
+				),
+				duplicates: new Set(
+					duplicates
+						.split(",")
+						.filter(el => el !== "")
+						.map(el => padZeros(el)),
+				),
+			};
+
+			results.set(res.username, res);
+		}
 	}
-	if (elements.length > 0) {
-		await scrape({ results, albumId, pagesCount: ++pagesCount });
+
+	return results;
+};
+
+const scrapeAlbumPage = async ({ albumId, pageNumber }) => {
+	const url = `http://slicice.net/search/albums.html?go=true&id=${albumId}&position=${pageNumber}`;
+	return scrapePage({ url });
+};
+
+const scrapePage = async ({ url, method = "GET", headers, data }) => {
+	console.log(`\nCalling: ${url}`);
+	const res = await axios({
+		url,
+		method,
+		headers,
+		data,
+	});
+	if (!JSESSIONID && Array.isArray(res.headers["set-cookie"]) && res.headers["set-cookie"].length > 0) {
+		JSESSIONID = res.headers["set-cookie"][0].split(";")[0];
 	}
+	return new JSDOM(res.data);
+};
+
+const getValidPagesNumbers = ({ albumId, page }) => {
+	const numbers = new Set();
+	const validLinks = page.window.document.querySelectorAll(
+		`a[href^='/search/albums.html?go=true&id=${albumId}&position=']`,
+	);
+	for (const a of validLinks) {
+		numbers.add(a.href.replace(`/search/albums.html?go=true&id=${albumId}&position=`, ""));
+	}
+	return [...numbers];
 };
 
 const autoTrade = ({ results, userData }) => {
-	const userDataCopy = cloneDeep(userData);
+	const trades = new Map();
 
-	const bestMatch = getBestMatch({ results, userData });
-	if (!bestMatch) {
-		return;
+	const resultsCopy = cloneDeep(results);
+	let userDataCopy = cloneDeep(userData);
+
+	while (true) {
+		userDataCopy = cloneDeep(userDataCopy);
+		const bestMatch = getBestMatch({ results: resultsCopy, userData: userDataCopy });
+		if (!bestMatch) {
+			break;
+		}
+
+		const username = bestMatch["username"];
+		const matchedMyMissing = bestMatch["matchedMyMissing"];
+		const matchedMyDuplicates = bestMatch["matchedMyDuplicates"];
+		const matchScore = bestMatch["matchScore"];
+
+		resultsCopy.delete(username);
+
+		const toReceive = [...matchedMyMissing].slice(0, matchScore).map(card => {
+			userDataCopy.missing.delete(card);
+			return card;
+		});
+		const toGive = [...matchedMyDuplicates].slice(0, matchScore).map(card => {
+			userDataCopy.duplicates.delete(card);
+			return card;
+		});
+
+		const text = `
+			<p>Zdravo!</p>
+			<p>Bi menjal z mano slicice? Menjava lahko ${matchScore} slicic.</p>
+			<p>Jaz imam zate:</p>
+			<p>${toGive.join(", ")}</p>
+			<p>Ti imas zame:</p>
+			<p>${toReceive.join(", ")}</p>
+			<p>Sporoci ce si za oziroma ce se imas/rabis navedene slicice. Imas lepo ohranjene slicice? Moj postni naslov je:</p>
+			<p>xxx</p>
+			<p>LP</p>
+		`;
+
+		console.log(`\nTRADING WITH USER '${username}'`);
+		console.log(`${matchScore} CARDS TO GIVE     :  ${toGive.join(", ")}`);
+		console.log(`${matchScore} CARDS TO RECEIVE  :  ${toReceive.join(", ")}`);
+		console.log(
+			`After this trade you are now missing ${padZeros(userDataCopy.missing.size)} cards and have ${padZeros(
+				userDataCopy.duplicates.size,
+			)} unique cards available for trade.`,
+		);
+
+		trades.set(username, text);
 	}
-	results.delete(bestMatch.username);
 
-	const toReceive = [...bestMatch.matchedMyMissing].slice(0, bestMatch.matchScore).map(card => {
-		userDataCopy.missing.delete(card);
-		return card;
-	});
-	const toGive = [...bestMatch.matchedMyDuplicates].slice(0, bestMatch.matchScore).map(card => {
-		userDataCopy.duplicates.delete(card);
-		return card;
-	});
-
-	console.log(`\nTRADING WITH USER '${bestMatch.username}'`);
-	console.log(`${bestMatch.matchScore} CARDS TO GIVE     :  ${toGive.join(", ")}`);
-	console.log(`${bestMatch.matchScore} CARDS TO RECEIVE  :  ${toReceive.join(", ")}`);
-	console.log(
-		`After this trade you are now missing ${padZeros(userDataCopy.missing.size)} cards and have ${padZeros(
-			userDataCopy.duplicates.size,
-		)} unique cards available for trade.`,
-	);
-
-	autoTrade({ results, userData: userDataCopy });
+	return trades;
 };
 
 const getBestMatch = ({ results, userData }) => {
@@ -124,12 +205,53 @@ const getBestMatch = ({ results, userData }) => {
 
 		result.matchScore = Math.min(result.matchedMyMissing.size, result.matchedMyDuplicates.size);
 
-		if (result.matchScore > 0 && (!bestMatch || bestMatch.matchScore < result.matchScore)) {
+		if (result.matchScore > 0 && (!bestMatch || bestMatch["matchScore"] < result.matchScore)) {
 			bestMatch = result;
 		}
 	}
 
 	return bestMatch;
+};
+
+const sendMessages = async ({ trades, username, password, doSendPrivateMessages }) => {
+	const loginUrl = `http://slicice.net/sw4i/login`;
+	const loginRes = await scrapePage({
+		url: loginUrl,
+		method: "POST",
+		headers: {
+			"Content-Type": "application/x-www-form-urlencoded",
+			Cookie: JSESSIONID,
+		},
+		data: querystring.encode({ username, password }),
+	});
+
+	const loginCheck = loginRes.window.document.querySelector(
+		"#header > div.userNav > ul > li:nth-child(1) > a > span",
+	);
+	if (!(loginCheck && loginCheck.textContent === username)) {
+		console.error("Script did not login correctly for your account.");
+	}
+
+	// send message to yourself
+	const privateMessageUrl = `http://slicice.net/sw4i/newPrivateMessage`;
+	for (const [username, text] of trades) {
+		if (doSendPrivateMessages) {
+			await scrapePage({
+				url: privateMessageUrl,
+				method: "POST",
+				headers: {
+					"Content-Type": "application/x-www-form-urlencoded",
+					Cookie: JSESSIONID,
+				},
+				data: querystring.encode({
+					receiver: username,
+					subject: "MENJAVA SLICIC",
+					body: text,
+				}),
+			});
+		}
+		console.log(text);
+	}
 };
 
 const padZeros = number => {
